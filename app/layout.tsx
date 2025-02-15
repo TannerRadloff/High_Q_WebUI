87f8ce8496c1455db0d27baea7e3d2a8eedd3f94
TannerRadloff <124326814+TannerRadloff@users.noreply.github.com>
2025-02-14 20:18:33 -0700

    Update layout.tsx
    Update to visuals of UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
-import type { Metadata } from 'next';
-import { Toaster } from 'sonner';
-
-import { ThemeProvider } from '@/components/theme-provider';
-
-import './globals.css';
+import type { Metadata } from 'next'
+import { Toaster } from 'sonner'
+import { ThemeProvider } from '@/components/theme-provider'
+import './globals.css'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
   title: 'Next.js Chatbot Template',
   description: 'Next.js chatbot template using the AI SDK.',
-};
+}
 
 export const viewport = {
-  maximumScale: 1, // Disable auto-zoom on mobile Safari
-};
+  maximumScale: 1,
+}
 
-const LIGHT_THEME_COLOR = 'hsl(0 0% 100%)';
-const DARK_THEME_COLOR = 'hsl(240deg 10% 3.92%)';
+const LIGHT_THEME_COLOR = 'hsl(0 0% 100%)'
+const DARK_THEME_COLOR = 'hsl(240deg 10% 3.92%)'
 const THEME_COLOR_SCRIPT = `\
 (function() {
   var html = document.documentElement;
@@ -33,30 +31,27 @@ const THEME_COLOR_SCRIPT = `\
   var observer = new MutationObserver(updateThemeColor);
   observer.observe(html, { attributes: true, attributeFilter: ['class'] });
   updateThemeColor();
-})();`;
+})();`
 
 export default async function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html
-      lang="en"
-      // `next-themes` injects an extra classname to the body element to avoid
-      // visual flicker before hydration. Hence the `suppressHydrationWarning`
-      // prop is necessary to avoid the React hydration mismatch warning.
-      // https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
-      suppressHydrationWarning
-    >
+    <html lang="en" suppressHydrationWarning className="font-body">
       <head>
         <script
           dangerouslySetInnerHTML={{
             __html: THEME_COLOR_SCRIPT,
           }}
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&family=Inter:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <body className="antialiased">
+      <body className="bg-brandBg text-brandNeutral antialiased">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -68,5 +63,5 @@ export default async function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
