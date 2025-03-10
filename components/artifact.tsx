b3b8c673554b41f4c9d91490d2c85e503e53e7ee
TannerRadloff <124326814+TannerRadloff@users.noreply.github.com>
2025-03-09 19:24:47 -0700

    Update artifact.tsx

--- components/artifact.tsx
+++ components/artifact.tsx
@@ -269,7 +269,7 @@ function PureArtifact({
     <AnimatePresence>
       {artifact.isVisible && (
         <motion.div
-          className="flex flex-row h-dvh w-dvw fixed top-0 left-0 z-50 bg-transparent"
+          className="flex flex-row h-dvh w-dvw fixed top-0 left-0 z-50 bg-gray-200 dark:bg-gray-800"
           initial={{ opacity: 1 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0, transition: { delay: 0.4 } }}
