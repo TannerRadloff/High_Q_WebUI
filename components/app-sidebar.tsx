d5b20338c4db60c103f12ced4ad86cbf0da6bfdf
HighQ <user@example.com>
2025-03-11 16:28:22 -0700

    Update app-sidebar.tsx

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -35,7 +35,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               className="flex flex-row gap-3 items-center"
             >
               <span className="text-lg font-semibold px-2 hover:bg-primary/10 hover:text-primary rounded-md cursor-pointer">
-                Chatbot
+                HighQ - Beta
               </span>
             </Link>
             <Tooltip>
