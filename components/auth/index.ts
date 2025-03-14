f767d9af928b992847bfb8733a86b0ae03d9c2a3
TannerRadloff <tannerradloff@gmail.com>
2025-03-14 00:55:01 -0700

    Create index.ts

---
+++ components/auth/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Auth Components
+ * 
+ * This barrel file exports all authentication-related components
+ * These components handle user authentication and authorization
+ */
+
+export * from './forgot-password-form';
+export * from './reset-password-form'; 
