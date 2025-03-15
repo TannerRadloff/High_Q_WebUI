6b34507a67af02177725e8c1176527bcc8cd89ec
TannerRadloff <tannerradloff@gmail.com>
2025-03-14 18:52:38 -0700

    Update sidebar.tsx

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -151,7 +151,7 @@ const SidebarProvider = React.forwardRef<
               } as React.CSSProperties
             }
             className={cn(
-              'group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar',
+              'group/sidebar-wrapper contents has-[[data-variant=inset]]:bg-sidebar',
               className,
             )}
             ref={ref}
